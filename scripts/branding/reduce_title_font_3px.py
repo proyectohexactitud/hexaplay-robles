e0479c82400a8250adbdd3f9da041163cc92af93
proyectohexactitud <proyectofinalpromo25@gmail.com>
2025-11-11 10:19:21 -0800

    feat(branding): add reduce_title_font_3px script

---
+++ scripts/branding/reduce_title_font_3px.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# Script placeholder to reduce the title font size by 3 pixels in the embedded HTML fragment of the autoapp binary.
+# TODO: Implement font-size reduction when necessary.
+print("TODO: implement reduction -3px in the HTML fragment")
